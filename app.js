12235f3e188edfecb52d9c5608476cf1d63ee197
tepandey <pandey.tek@gmail.com>
2021-08-17 08:40:52 -0700

    Created package.json, app.js examples

---
+++ app.js
@@ -0,0 +1,21 @@
+const myObject = {
+    a: "this is property a",
+    mySeconObjectProperty: "second object property",
+    "x": "the x key on my object",
+    "2": "the key that is number 2",
+    3: "ket that is 3"
+}
+
+console.log("my object, myObject");
+
+const myInfo ={
+    "name": "Adam",
+    "birthday":08191983,
+    "citiesLivedIn": ["chicago", "Los Angeles"],
+    "isHuman":true,
+    "planetVisited": null,
+    "personalInfo":{
+    "favoritefood": "hamburger",
+    }
+};
+console.log(myInfo);
